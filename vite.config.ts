b045793f4e903d5665e359d75258850d72169483
liebenjoy <joylieben@gmail.com>
2025-08-05 17:18:43 -0400

    Configure GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/LiebenPortfolioSite/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
